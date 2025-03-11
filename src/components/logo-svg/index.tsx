398d50a48dbfadc2a32eac7ff4e12b530e19e841
Willian Alves Moreno <willianam@uninove.br>
2025-03-11 09:55:30 -0300

    feat: Criando componente logo svg

---
+++ src/components/logo-svg/index.tsx
@@ -0,0 +1,159 @@
+import Svg, { G, Path, Polygon, RadialGradient, Stop, SvgProps } from 'react-native-svg'
+
+export function LogoSvg(props: SvgProps) {
+  return (
+    <Svg id="Layer_1" x="0px" y="0px" viewBox="0 0 95.7 28" {...props}>
+      <G>
+        <G>
+          <Path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            fill="#004588"
+            d="M7,1.6l-1.5,7c-0.4,2-1.2,5.6,4.6,5.6c1.6,0,3.8-0.3,5.3-1.8c1-0.9,1.5-2.4,1.8-3.8l1.5-7.1h-3.3l-1.5,7.2 c-0.3,1.2-0.9,3-3.1,3c-0.8,0-1.6-0.3-1.9-1c-0.2-0.5-0.1-1.2,0-1.9l1.6-7.3L7,1.6L7,1.6z"
+          />
+          <Polygon
+            fillRule="evenodd"
+            clipRule="evenodd"
+            fill="#004588"
+            points="18.5,13.9 21.5,13.9 23.5,4.3 25.8,13.9 30.4,13.9 33,1.6 30,1.6 28.1,10.8 25.9,1.6 21.2,1.6  "
+          />
+          <Polygon
+            fillRule="evenodd"
+            clipRule="evenodd"
+            fill="#004588"
+            points="32.6,13.9 36.2,13.9 38.8,1.6 35.3,1.6  "
+          />
+          <Polygon
+            fillRule="evenodd"
+            clipRule="evenodd"
+            fill="#004588"
+            points="38.5,13.9 41.5,13.9 43.5,4.3 45.7,13.9 50.3,13.9 52.9,1.6 49.9,1.6 48,10.8 45.8,1.6 41.1,1.6  "
+          />
+          <Path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            fill="#004588"
+            d="M53.5,7.8c-1,4.6,1.7,6.4,5,6.4c2.9,0,6.7-1.5,7.8-6.4c0.6-3,0.1-6.5-4.9-6.5c-0.9,0-3.3,0.1-5.2,1.7 C54.4,4.5,53.7,7,53.5,7.8L53.5,7.8z M57,7.8c0.5-2.1,1.5-4.1,3.7-4.1c0.8,0,1.5,0.2,1.9,0.9c0.3,0.6,0.5,1.4,0.1,3.1 c-0.4,1.7-1.3,4-3.7,4C56.8,11.7,56.6,9.8,57,7.8L57,7.8z"
+          />
+          <Polygon
+            fillRule="evenodd"
+            clipRule="evenodd"
+            fill="#004588"
+            points="69.6,13.9 73.7,13.9 80.8,1.6 77.4,1.6 72.5,10.5 71.6,1.6 67.9,1.6  "
+          />
+          <Polygon
+            fillRule="evenodd"
+            clipRule="evenodd"
+            fill="#004588"
+            points="79.6,13.9 88.9,13.9 89.5,11.5 83.4,11.5 84,8.9 89.5,8.9 90,6.5 84.5,6.5 85.1,4 90.9,4 91.4,1.6  82.2,1.6  "
+          />
+        </G>
+        <RadialGradient
+          id="SVGID_1_"
+          cx={8.5226}
+          cy={9.1617}
+          r={5.3368}
+          gradientTransform="matrix(1 0 0 -1 0 30)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop offset={0} stopColor="#E75236" />
+          <Stop offset={0.0954} stopColor="#E64431" />
+          <Stop offset={0.33} stopColor="#E42627" />
+          <Stop offset={0.5618} stopColor="#E21120" />
+          <Stop offset={0.7874} stopColor="#E1041B" />
+          <Stop offset={1} stopColor="#E1001A" />
+        </RadialGradient>
+        <Path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          fill="url(#SVGID_1_)"
+          d="M10.8,17.8c3.6,0,6.5,2,6.5,4.5c0,2.4-2.9,4.4-6.5,4.4s-6.5-2-6.5-4.4C4.2,19.9,7.1,17.8,10.8,17.8L10.8,17.8z "
+        />
+        <RadialGradient
+          id="SVGID_00000145031974387231119480000002405221049648782496_"
+          cx={26.4689}
+          cy={9.1617}
+          r={5.3363}
+          gradientTransform="matrix(1 0 0 -1 0 30)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop offset={0} stopColor="#E75236" />
+          <Stop offset={0.0954} stopColor="#E64431" />
+          <Stop offset={0.33} stopColor="#E42627" />
+          <Stop offset={0.5618} stopColor="#E21120" />
+          <Stop offset={0.7874} stopColor="#E1041B" />
+          <Stop offset={1} stopColor="#E1001A" />
+        </RadialGradient>
+        <Path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          fill="url(#SVGID_00000145031974387231119480000002405221049648782496_)"
+          d=" M28.7,17.8c3.6,0,6.5,2,6.5,4.5c0,2.4-2.9,4.4-6.5,4.4c-3.6,0-6.5-2-6.5-4.4C22.2,19.9,25.1,17.8,28.7,17.8L28.7,17.8z"
+        />
+        <RadialGradient
+          id="SVGID_00000079444940543121426810000005873386463802004925_"
+          cx={44.573}
+          cy={9.1617}
+          r={5.3364}
+          gradientTransform="matrix(1 0 0 -1 0 30)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop offset={0} stopColor="#E75236" />
+          <Stop offset={0.0954} stopColor="#E64431" />
+          <Stop offset={0.33} stopColor="#E42627" />
+          <Stop offset={0.5618} stopColor="#E21120" />
+          <Stop offset={0.7874} stopColor="#E1041B" />
+          <Stop offset={1} stopColor="#E1001A" />
+        </RadialGradient>
+        <Path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          fill="url(#SVGID_00000079444940543121426810000005873386463802004925_)"
+          d=" M46.8,17.8c3.6,0,6.5,2,6.5,4.5c0,2.4-2.9,4.4-6.5,4.4s-6.5-2-6.5-4.4C40.3,19.9,43.2,17.8,46.8,17.8L46.8,17.8z"
+        />
+        <RadialGradient
+          id="SVGID_00000068663414963334119810000005648876621940154788_"
+          cx={63.306}
+          cy={9.1617}
+          r={5.3364}
+          gradientTransform="matrix(1 0 0 -1 0 30)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop offset={0} stopColor="#E75236" />
+          <Stop offset={0.0954} stopColor="#E64431" />
+          <Stop offset={0.33} stopColor="#E42627" />
+          <Stop offset={0.5618} stopColor="#E21120" />
+          <Stop offset={0.7874} stopColor="#E1041B" />
+          <Stop offset={1} stopColor="#E1001A" />
+        </RadialGradient>
+        <Path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          fill="url(#SVGID_00000068663414963334119810000005648876621940154788_)"
+          d=" M65.5,17.8c3.6,0,6.5,2,6.5,4.5c0,2.4-2.9,4.4-6.5,4.4c-3.6,0-6.5-2-6.5-4.4C59,19.9,61.9,17.8,65.5,17.8L65.5,17.8z"
+        />
+        <RadialGradient
+          id="SVGID_00000102523638982023983300000003392413255431161762_"
+          cx={81.4096}
+          cy={9.1617}
+          r={5.3365}
+          gradientTransform="matrix(1 0 0 -1 0 30)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop offset={0} stopColor="#E75236" />
+          <Stop offset={0.0954} stopColor="#E64431" />
+          <Stop offset={0.33} stopColor="#E42627" />
+          <Stop offset={0.5618} stopColor="#E21120" />
+          <Stop offset={0.7874} stopColor="#E1041B" />
+          <Stop offset={1} stopColor="#E1001A" />
+        </RadialGradient>
+        <Path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          fill="url(#SVGID_00000102523638982023983300000003392413255431161762_)"
+          d=" M83.6,17.8c3.6,0,6.5,2,6.5,4.5c0,2.4-2.9,4.4-6.5,4.4s-6.5-2-6.5-4.4C77.1,19.9,80,17.8,83.6,17.8L83.6,17.8z"
+        />
+      </G>
+    </Svg>
+  )
+}
