d10b66993d4973697f2b2a50afb8886604b34b23
Willian Alves Moreno <willianam@uninove.br>
2025-03-12 12:54:29 -0300

    refactor: Atualizando config e utilizando status bar do expo

--- App.tsx
+++ App.tsx
@@ -3,14 +3,15 @@ import './global.css'
 import { Loading } from '@components/loading'
 import { OpenSans_400Regular, OpenSans_700Bold, useFonts } from '@expo-google-fonts/open-sans'
 import { Routes } from '@routes/index'
-import { StatusBar, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { View } from 'react-native'
 
 export function App() {
   const [fontsLoaded] = useFonts({ OpenSans_400Regular, OpenSans_700Bold })
 
   return (
     <View className="flex-1 bg-sky-100">
-      <StatusBar barStyle="dark-content" className="bg-sky-100" />
+      <StatusBar style="dark" backgroundColor="transparent" translucent />
       {fontsLoaded ? <Routes /> : <Loading />}
     </View>
   )
