fa81193159ea5f7f9733b84fccdcd161320c7df3
Willian Alves Moreno <willianam@uninove.br>
2025-03-12 12:53:44 -0300

    refactor: Adicionando wrapper component input text

--- src/components/input-text/index.tsx
+++ src/components/input-text/index.tsx
@@ -1,19 +1,21 @@
 import { cn } from '@utils/cn'
-import { TextInput, TextInputProps } from 'react-native'
+import { TextInput, TextInputProps, View } from 'react-native'
 
 type Props = {} & TextInputProps
 
 export function InputText({ className, ...props }: Props) {
   return (
-    <TextInput
-      className={cn(
-        'h-14 w-full rounded-sm bg-white px-4 text-lg text-sky-800 shadow shadow-slate-800/10',
-        'placeholder:text-slate-300',
-        className,
-      )}
-      autoCapitalize="none"
-      underlineColorAndroid="transparent"
-      {...props}
-    />
+    <View className="flex h-14 w-full flex-row gap-x-4 rounded-sm bg-white pl-4 shadow shadow-slate-800/10">
+      <TextInput
+        className={cn(
+          'h-14 flex-1 bg-white text-lg text-sky-800',
+          'placeholder:text-slate-300',
+          className,
+        )}
+        autoCapitalize="none"
+        underlineColorAndroid="transparent"
+        {...props}
+      />
+    </View>
   )
 }
