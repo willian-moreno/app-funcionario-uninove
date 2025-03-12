418cd6bbb199a9f218ff9004a210b41225b4ff99
Willian Alves Moreno <willianam@uninove.br>
2025-03-12 13:11:11 -0300

    feat: Criando layout telas autenticação

--- src/layouts/auth/index.tsx
+++ src/layouts/auth/index.tsx
@@ -1,12 +1,28 @@
-import { StatusBar, View, ViewProps } from 'react-native'
+import { Footer } from '@components/footer'
+import { KeyboardAvoiding } from '@components/keyboard-avoiding'
+import { LogoSvg } from '@components/logo-svg'
+import { StatusBar } from 'expo-status-bar'
+import { View } from 'react-native'
 
-type Props = {} & ViewProps
+type Props = {
+  children: React.ReactNode
+}
 
 export function AuthLayout({ children }: Props) {
   return (
-    <View className="flex-1 bg-sky-100 p-6">
-      <StatusBar barStyle="dark-content" className="bg-sky-100" />
-      {children}
-    </View>
+    <>
+      <StatusBar style="dark" backgroundColor="transparent" translucent />
+      <KeyboardAvoiding.Root className="bg-sky-100">
+        <KeyboardAvoiding.View>
+          <KeyboardAvoiding.Scroll>
+            <View className="mx-auto flex w-52 flex-1">
+              <LogoSvg height={100} style={{ marginTop: 'auto', marginBottom: 'auto' }} />
+            </View>
+            {children}
+            <Footer variant="auth" />
+          </KeyboardAvoiding.Scroll>
+        </KeyboardAvoiding.View>
+      </KeyboardAvoiding.Root>
+    </>
   )
 }
