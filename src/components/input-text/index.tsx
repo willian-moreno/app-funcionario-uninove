2a70faedc129fd2e23e829cccf5054b54950d152
Willian Alves Moreno <willianam@uninove.br>
2025-03-12 10:02:40 -0300

    feat: Criando componente input text

---
+++ src/components/input-text/index.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@utils/cn'
+import { TextInput, TextInputProps } from 'react-native'
+
+type Props = {} & TextInputProps
+
+export function InputText({ className, ...props }: Props) {
+  return (
+    <TextInput
+      className={cn(
+        'h-14 w-full rounded-sm bg-white px-4 text-lg text-sky-800 shadow-xl shadow-black/25',
+        'placeholder:text-slate-300',
+        className,
+      )}
+      autoCapitalize="none"
+      underlineColorAndroid="transparent"
+      {...props}
+    />
+  )
+}
