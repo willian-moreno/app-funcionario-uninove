22c589b2b2c4c38b9ce2573be7b078750c9d37f0
Willian Alves Moreno <willianam@uninove.br>
2025-03-12 12:36:13 -0300

    style: Removendo classes não funcionais

--- src/components/input-password/index.tsx
+++ src/components/input-password/index.tsx
@@ -13,7 +13,7 @@ export function InputPassword({ className, ...props }: Props) {
   }
 
   return (
-    <View className="flex h-14 w-full flex-row gap-x-4 rounded-sm bg-white pl-4 text-lg text-sky-800 shadow shadow-slate-800/10">
+    <View className="flex h-14 w-full flex-row gap-x-4 rounded-sm bg-white pl-4 shadow shadow-slate-800/10">
       <TextInput
         className={cn(
           'h-14 flex-1 bg-white text-lg text-sky-800',
