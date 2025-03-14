28e345d875748103e5d86821cc6898309d296e1a
Willian Alves Moreno <willianam@uninove.br>
2025-03-14 16:19:59 -0300

    refactor: Ajustando cores componente loading

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -2,8 +2,8 @@ import { ActivityIndicator, View } from 'react-native'
 
 export function Loading() {
   return (
-    <View className="flex-1 items-center justify-center bg-sky-100/70">
-      <ActivityIndicator className="text-black" size={32} />
+    <View className="flex-1 items-center justify-center bg-sky-50/70">
+      <ActivityIndicator className="text-sky-800" size={32} />
     </View>
   )
 }
