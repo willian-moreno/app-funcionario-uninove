0c685b2097c60512e83039fd724a0bb3a375cdef
Willian Alves Moreno <willianam@uninove.br>
2025-03-17 11:30:54 -0300

    refactor: Utilizando componente de profile

--- src/screens/authenticated-area/employee-on-vacation/index.tsx
+++ src/screens/authenticated-area/employee-on-vacation/index.tsx
@@ -1,5 +1,6 @@
+import { ProfileButton } from '@components/profile-button'
 import { VacancieSvg } from '@components/vacancie-svg'
-import { Dimensions, Text, TouchableOpacity, View } from 'react-native'
+import { Dimensions, Text, View } from 'react-native'
 
 export function EmployeeOnVacation() {
   const windowWidth = Math.round(Dimensions.get('window').width)
@@ -10,15 +11,7 @@ export function EmployeeOnVacation() {
 
   return (
     <View className="flex-1 gap-5">
-      <View className="flex-row items-center gap-2">
-        <TouchableOpacity
-          className="aspect-square h-16 w-16 items-center justify-center rounded-full border-4 border-slate-300 bg-sky-800"
-          activeOpacity={0.7}
-        >
-          <Text className="text-xl font-semibold text-white">WM</Text>
-        </TouchableOpacity>
-        <Text className="font-sans-bold text-2xl text-sky-800">Olá, Willian</Text>
-      </View>
+      <ProfileButton userName="Willian Moreno" onPress={() => {}} />
       <View className="mt-auto gap-2">
         <Text className="font-sans-bold text-xl text-sky-800">
           Aproveite suas férias para descansar e recarregar as energias.
