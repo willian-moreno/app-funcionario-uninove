b503876d3508704e0365048a3473315cef16757a
Willian Alves Moreno <willianam@uninove.br>
2025-03-17 16:16:49 -0300

    feat: Criando componente separator

---
+++ src/components/separator/index.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@utils/cn'
+import { View, ViewProps } from 'react-native'
+
+type Props = {
+  orientation?: 'horizontal' | 'vertical'
+} & ViewProps
+
+export function Separator({ orientation = 'horizontal', className, ...props }: Props) {
+  return (
+    <View
+      className={cn('bg-slate-200', className, {
+        'h-0.5 w-full': orientation === 'horizontal',
+        'h-full w-0.5': orientation === 'vertical',
+      })}
+      {...props}
+    />
+  )
+}
