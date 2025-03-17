47113c31ae9c8d04da28c390c5050d3b25189746
Willian Alves Moreno <willianam@uninove.br>
2025-03-17 19:20:58 -0300

    style: Atualizando padding area autenticada

--- src/layouts/authenticated-area/index.tsx
+++ src/layouts/authenticated-area/index.tsx
@@ -10,8 +10,11 @@ export function AuthenticatedArea({ children }: Props) {
   return (
     <>
       <StatusBar style="dark" backgroundColor="#f1f5f9" />
-      <View className="flex-1 bg-slate-100 p-6">
-        <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ flexGrow: 1 }}>
+      <View className="flex-1 bg-slate-100">
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ flexGrow: 1, padding: 24 }}
+        >
           {children}
           <Footer variant="default" />
         </ScrollView>
