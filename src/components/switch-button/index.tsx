e9d0425a3bc01bc02fb87ff0d427c06050807d95
Willian Alves Moreno <willianam@uninove.br>
2025-03-17 17:53:35 -0300

    feat: Criando componente switch

---
+++ src/components/switch-button/index.tsx
@@ -0,0 +1,48 @@
+import { cn } from '@utils/cn'
+import { useState } from 'react'
+import { TouchableOpacity, TouchableOpacityProps, View } from 'react-native'
+
+type Props = {
+  value?: boolean
+  onChangeValue?: (isActive: boolean) => void
+} & TouchableOpacityProps
+
+export function SwitchButton({
+  className,
+  disabled,
+  value = false,
+  onChangeValue = () => {},
+  ...props
+}: Props) {
+  const [isActive, setIsActive] = useState(value)
+
+  function handleToggleActivation() {
+    if (disabled) {
+      return
+    }
+
+    const newState = !isActive
+
+    setIsActive(newState)
+    onChangeValue(newState)
+  }
+
+  return (
+    <TouchableOpacity
+      className={cn('h-8 w-14 rounded-full bg-slate-300 p-1', className, {
+        'bg-sky-800': isActive && !disabled,
+        'bg-sky-800/20': isActive && disabled,
+        'bg-slate-200': !isActive && disabled,
+      })}
+      onPress={handleToggleActivation}
+      activeOpacity={disabled ? 1 : 0.7}
+      {...props}
+    >
+      <View
+        className={cn('aspect-square h-full rounded-full bg-white shadow-sm', {
+          'ml-auto': isActive,
+        })}
+      />
+    </TouchableOpacity>
+  )
+}
