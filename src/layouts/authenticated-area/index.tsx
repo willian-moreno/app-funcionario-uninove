ee3466ab68908f32df0132628c654f302256553a
Willian Alves Moreno <willianam@uninove.br>
2025-03-17 11:57:11 -0300

    feat: Criando layout area autenticada

---
+++ src/layouts/authenticated-area/index.tsx
@@ -0,0 +1,19 @@
+import { Footer } from '@components/footer'
+import { StatusBar } from 'expo-status-bar'
+import { View } from 'react-native'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export function AuthenticatedArea({ children }: Props) {
+  return (
+    <>
+      <StatusBar style="dark" backgroundColor="#f1f5f9" />
+      <View className="flex-1 bg-slate-100 p-6">
+        {children}
+        <Footer variant="default" />
+      </View>
+    </>
+  )
+}
