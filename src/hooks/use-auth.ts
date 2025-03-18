937dfe5c6ed3a707866e07bfed9f20db9d0d7f6e
Willian Alves Moreno <willian_moreno@outlook.com>
2025-03-18 14:00:19 -0300

    refactor: Alterando hook auth

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -5,7 +5,13 @@ import { removeAuthStorage } from '@storage/auth/remove-auth-storage'
 import { removeProfileStorage } from '@storage/auth/remove-profile-storage'
 import { useEffect, useState } from 'react'
 
-type Auth = { user: User; accessToken: string }
+type Auth = {
+  user: {
+    firstName?: string
+    nameInitials?: string
+  } & User
+  accessToken: string
+}
 
 export function useAuth() {
   const navigation = useNavigation()
@@ -13,8 +19,6 @@ export function useAuth() {
   const [isLoading, setIsLoading] = useState(true)
 
   const [auth, setAuth] = useState<Auth>()
-
-  const firstName = auth?.user.fullName.split(' ')[0] ?? ''
 
   async function findStoredAuth() {
     try {
@@ -26,7 +30,22 @@ export function useAuth() {
         return
       }
 
-      setAuth(auth)
+      const firstUserName = auth?.user.fullName.split(' ')[0] ?? ''
+
+      const userNameInitials = auth?.user.fullName
+        .split(' ')
+        .slice(0, 2)
+        .map((name) => name.charAt(0).toLocaleUpperCase())
+        .join('')
+
+      setAuth({
+        ...auth,
+        user: {
+          ...auth.user,
+          firstName: firstUserName,
+          nameInitials: userNameInitials,
+        },
+      })
     } catch (error) {
     } finally {
       setIsLoading(false)
@@ -51,7 +70,7 @@ export function useAuth() {
   }, [])
 
   return {
-    auth: { ...auth, user: { ...auth?.user, firstName } },
+    auth,
     isLoading,
     signOut,
   }
