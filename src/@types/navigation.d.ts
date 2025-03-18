3b166c25b1ab9fe958ff17d42cbe7401bc75c0db
Willian Alves Moreno <willianam@uninove.br>
2025-03-18 12:44:42 -0300

    refactor: Adicionando tipagem rota impressão digital

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -2,6 +2,7 @@ export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       sign_in: undefined
+      fingerprint_sign_in: undefined
       reset_password_first_stage: undefined
       reset_password_second_stage: undefined
       reset_password_third_stage: undefined
