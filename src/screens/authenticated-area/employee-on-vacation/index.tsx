c88523d4ad56df5372a9f51bd6277cf028dcdfe5
Willian Alves Moreno <willian_moreno@outlook.com>
2025-03-18 13:34:53 -0300

    refactor: Utilizando hook auth

--- src/screens/authenticated-area/employee-on-vacation/index.tsx
+++ src/screens/authenticated-area/employee-on-vacation/index.tsx
@@ -1,19 +1,30 @@
+import { Loading } from '@components/loading'
 import { ProfileButton } from '@components/profile-button'
 import { VacancieSvg } from '@components/vacancie-svg'
+import { useAuth } from '@hooks/use-auth'
 import { Dimensions, Text, View } from 'react-native'
 
 export function EmployeeOnVacation() {
+  const {
+    auth: { user },
+    isLoading: isScreenLoading,
+  } = useAuth()
+
   const windowWidth = Math.round(Dimensions.get('window').width)
 
   const svgDecreaseLeft = windowWidth * 0.3
   const svgWidth = windowWidth + windowWidth * 0.25
   const svgHeight = svgWidth * 0.85
 
+  if (isScreenLoading) {
+    return <Loading />
+  }
+
   return (
     <View className="flex-1 gap-5">
       <View className="flex-row items-center gap-2">
-        <ProfileButton fullUserName="Willian Alves Moreno" onPress={() => {}} />
-        <Text className="font-sans-bold text-2xl text-sky-800">Olá, Willian</Text>
+        <ProfileButton fullUserName={user?.fullName} onPress={() => {}} />
+        <Text className="font-sans-bold text-2xl text-sky-800">Olá, {user.firstName}</Text>
       </View>
       <View className="mt-auto gap-2">
         <Text className="font-sans-bold text-xl text-sky-800">
