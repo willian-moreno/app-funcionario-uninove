f141aa134df70dacf7bc4b499ec5c3953f69ebc2
willian-moreno <willian_moreno@outlook.com>
2025-03-22 15:31:13 -0300

    feat: Adicionando react native svg transformer

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,17 @@ const { withNativeWind } = require('nativewind/metro')
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname)
 
+const { transformer, resolver } = config
+
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
+}
+
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...resolver.sourceExts, "svg"]
+}
+
 module.exports = withNativeWind(config, { input: './global.css' })
