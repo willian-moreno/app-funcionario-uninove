8d981b14eb5e6cc62b0e14e3f65b686ce730cfd8
Willian Alves Moreno <willian_moreno@outlook.com>
2025-03-24 12:31:10 -0300

    feat: Criando seção benefícios e serviços home

--- src/screens/authenticated-area/home/index.tsx
+++ src/screens/authenticated-area/home/index.tsx
@@ -4,23 +4,41 @@ import { Loading } from '@components/loading'
 import { ProfileButton } from '@components/profile-button'
 import { ScreenScrollView } from '@components/screen-scroll-view'
 import { AuthContext } from '@contexts/auth-context-provider'
+import BookOutlined from '@material-symbols/svg-500/outlined/book.svg'
+import DentistryOutlined from '@material-symbols/svg-500/outlined/dentistry.svg'
+import EmojiLanguageOutlined from '@material-symbols/svg-500/outlined/emoji_language.svg'
+import MedicalServicesOutlined from '@material-symbols/svg-500/outlined/medical_services.svg'
 import NotificationsOutlined from '@material-symbols/svg-500/outlined/notifications.svg'
+import RestaurantOutlined from '@material-symbols/svg-500/outlined/restaurant.svg'
+import TheaterComedyOutlined from '@material-symbols/svg-500/outlined/theater_comedy.svg'
 import { useNavigation } from '@react-navigation/native'
-import { cssInterop } from 'nativewind'
+import { svgCssInterop } from '@utils/svg-css-interop'
 import { useContext } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
 
-cssInterop(NotificationsOutlined, {
-  className: {
-    target: 'style',
-    nativeStyleToProp: { width: true, height: true, fill: true },
-  },
-})
+svgCssInterop([
+  BookOutlined,
+  DentistryOutlined,
+  EmojiLanguageOutlined,
+  MedicalServicesOutlined,
+  NotificationsOutlined,
+  RestaurantOutlined,
+  TheaterComedyOutlined,
+])
 
 export function Home() {
   const navigation = useNavigation()
 
   const { auth, isLoading: isScreenLoading } = useContext(AuthContext)
+
+  const benefitsAndServices = [
+    { title: 'NotreDame Intermédica', Icon: MedicalServicesOutlined, onPress: () => {} },
+    { title: 'Metlife', Icon: DentistryOutlined, onPress: () => {} },
+    { title: 'Ticke Edenred', Icon: RestaurantOutlined, onPress: () => {} },
+    { title: 'Sesc', Icon: TheaterComedyOutlined, onPress: () => {} },
+    { title: 'Voxy', Icon: EmojiLanguageOutlined, onPress: () => {} },
+    { title: 'ExLibris', Icon: BookOutlined, onPress: () => {} },
+  ]
 
   async function handleNavigateToMyProfileScreen() {
     navigation.navigate('my_profile')
@@ -35,10 +53,10 @@ export function Home() {
   }
 
   return (
-    <ScreenScrollView>
+    <ScreenScrollView contentContainerClassName="p-0 py-6">
       <View className="flex-1 gap-y-6">
-        <View className="flex-row items-center justify-between">
-          <View className="flex-row items-center gap-2">
+        <View className="flex-row items-center justify-between gap-x-6 px-6">
+          <View className="flex-row items-center gap-x-2">
             <ProfileButton
               userNameInitials={auth.user.nameInitials}
               onPress={handleNavigateToMyProfileScreen}
@@ -55,6 +73,26 @@ export function Home() {
             </TouchableOpacity>
             <Bedge.Dot />
           </Bedge.Root>
+        </View>
+        <View className="gap-y-6">
+          <Text className="px-6 font-sans-bold text-2xl text-sky-900">Benefícios e serviços</Text>
+          <ScrollView
+            contentContainerClassName="gap-x-3 px-6"
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          >
+            {benefitsAndServices.map(({ title, Icon, onPress }, index) => (
+              <TouchableOpacity
+                key={title}
+                className="aspect-square w-[35vw] justify-between gap-y-6 rounded-sm bg-sky-100 p-4"
+                activeOpacity={0.7}
+                onPress={onPress}
+              >
+                <Icon className="h-8 w-8 fill-sky-400" />
+                <Text className="font-sans-bold text-lg leading-tight text-sky-900">{title}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
         </View>
       </View>
       <Footer />
