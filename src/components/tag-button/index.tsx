29e5d9398ddb1754fd3a04805b0321964eec0960
Willian Alves Moreno <willian_moreno@outlook.com>
2025-03-24 18:26:11 -0300

    feat: Criando componente tag button

---
+++ src/components/tag-button/index.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@utils/cn'
+import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+
+type Props = {
+  value?: string
+} & TouchableOpacityProps
+
+export function TagButton({ children, className, value = '', ...props }: Props) {
+  return (
+    <TouchableOpacity
+      className={cn(
+        'items-center justify-center rounded-full bg-sky-100 px-4 py-1 uppercase shadow shadow-sky-800/70',
+        className,
+      )}
+      activeOpacity={0.7}
+      {...props}
+    >
+      {children ? (
+        children
+      ) : (
+        <Text className="mx-auto font-sans-bold text-sm text-sky-900">{value}</Text>
+      )}
+    </TouchableOpacity>
+  )
+}
