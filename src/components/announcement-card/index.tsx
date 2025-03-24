f7dd81d037696e45bd1458dda48fbbd94e38ff69
Willian Alves Moreno <willian_moreno@outlook.com>
2025-03-24 18:26:30 -0300

    feat: Criando componente announcement card

---
+++ src/components/announcement-card/index.tsx
@@ -0,0 +1,55 @@
+import { Announcement } from '@@types/announcement'
+import IOSShareOutlined from '@material-symbols/svg-600/outlined/ios_share.svg'
+import { cn } from '@utils/cn'
+import { svgCssInterop } from '@utils/svg-css-interop'
+import { formatInTimeZone } from 'date-fns-tz'
+import { ptBR } from 'date-fns/locale/pt-BR'
+import { Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native'
+
+svgCssInterop([IOSShareOutlined])
+
+type Props = {
+  announcement: Announcement
+} & TouchableOpacityProps
+
+export function AnnouncementCard({
+  announcement: { title, department, publishedAt, isNew },
+  ...props
+}: Props) {
+  function formatDate(date: string) {
+    return formatInTimeZone(date, 'America/Sao_Paulo', "dd 'de' MMMM 'de' yyyy 'às' HH:mm", {
+      locale: ptBR,
+    })
+  }
+
+  return (
+    <TouchableOpacity
+      className={cn('justify-end gap-y-6 bg-slate-200 p-6 shadow shadow-sky-800/70', {
+        'bg-sky-100': isNew,
+      })}
+      activeOpacity={0.7}
+      {...props}
+    >
+      <View className="gap-y-2">
+        {isNew && (
+          <Text className="mr-auto bg-sky-400 px-4 py-1 font-sans-semibold text-sm uppercase text-white">
+            Novo
+          </Text>
+        )}
+        <Text className="font-sans-bold text-2xl text-sky-900" numberOfLines={2}>
+          {title}
+        </Text>
+        <View>
+          <Text className="font-sans-bold text-lg text-sky-900">{department}</Text>
+          <Text className="font-sans-regular text-lg text-sky-900">{formatDate(publishedAt)}</Text>
+        </View>
+      </View>
+      <TouchableOpacity
+        className="mr-auto h-10 w-10 items-center justify-center"
+        activeOpacity={0.7}
+      >
+        <IOSShareOutlined className="h-8 w-8 fill-sky-900" onPress={() => {}} />
+      </TouchableOpacity>
+    </TouchableOpacity>
+  )
+}
