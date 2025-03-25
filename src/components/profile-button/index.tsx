0a1e63db055dc1de23eb1c768451a5557c9037f2
Willian Alves Moreno <willian_moreno@outlook.com>
2025-03-25 15:15:38 -0300

    feat: Adicionando propriedade size profile button

--- src/components/profile-button/index.tsx
+++ src/components/profile-button/index.tsx
@@ -1,17 +1,35 @@
+import { cn } from '@utils/cn'
 import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
 
 type Props = {
   userNameInitials?: string
+  size?: 'default' | 'large'
 } & TouchableOpacityProps
 
-export function ProfileButton({ userNameInitials = '', disabled, ...props }: Props) {
+export function ProfileButton({
+  userNameInitials = '',
+  size = 'default',
+  disabled,
+  ...props
+}: Props) {
   return (
     <TouchableOpacity
-      className="aspect-square h-16 w-16 items-center justify-center rounded-full border-4 border-slate-300 bg-sky-800 shadow shadow-sky-900/70"
+      className={cn(
+        'aspect-square w-16 items-center justify-center rounded-full border-4 border-slate-300 bg-sky-800 shadow shadow-sky-900/70',
+        {
+          'w-32': size === 'large',
+        },
+      )}
       activeOpacity={disabled ? 1 : 0.7}
       {...props}
     >
-      <Text className="text-xl font-semibold text-white">{userNameInitials}</Text>
+      <Text
+        className={cn('text-xl font-semibold text-white', {
+          'text-5xl leading-tight': size === 'large',
+        })}
+      >
+        {userNameInitials}
+      </Text>
     </TouchableOpacity>
   )
 }
