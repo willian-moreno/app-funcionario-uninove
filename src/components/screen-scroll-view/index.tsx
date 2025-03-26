042fa2b8dc56c8125609652db818eef492e2a376
Willian Alves Moreno <willian_moreno@outlook.com>
2025-03-26 16:26:49 -0300

    fix: Bug eventos não funcionando dentro scrollview

--- src/components/screen-scroll-view/index.tsx
+++ src/components/screen-scroll-view/index.tsx
@@ -1,17 +1,19 @@
 import { cn } from '@utils/cn'
-import { ScrollView, ScrollViewProps } from 'react-native'
+import { ScrollView, ScrollViewProps, View } from 'react-native'
 
 type Props = {} & ScrollViewProps
 
 export function ScreenScrollView({ children, contentContainerClassName, ...props }: Props) {
   return (
-    <ScrollView
-      showsVerticalScrollIndicator={false}
-      contentContainerClassName={cn('flex-grow p-6', contentContainerClassName)}
-      keyboardShouldPersistTaps="handled"
-      {...props}
-    >
-      {children}
-    </ScrollView>
+    <View className="flex-1">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName={cn('flex-grow p-6', contentContainerClassName)}
+        keyboardShouldPersistTaps="handled"
+        {...props}
+      >
+        {children}
+      </ScrollView>
+    </View>
   )
 }
