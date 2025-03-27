1927969b5d3d84fa8f7addb8e26dbe3b440749ff
Willian Alves Moreno <willian_moreno@outlook.com>
2025-03-27 16:01:39 -0300

    style: Adicionando variant component loading

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,12 +1,19 @@
 import { cn } from '@utils/cn'
 import { ActivityIndicator, View, ViewProps } from 'react-native'
 
-type Props = {} & ViewProps
+type Props = {
+  variant?: 'default' | 'light'
+} & ViewProps
 
-export function Loading({ className, ...props }: Props) {
+export function Loading({ className, variant = 'default', ...props }: Props) {
   return (
     <View className={cn('flex-1 items-center justify-center', className)} {...props}>
-      <ActivityIndicator className="text-sky-900" size={32} />
+      <ActivityIndicator
+        className={cn('text-sky-900', {
+          'text-white': variant === 'light',
+        })}
+        size={32}
+      />
     </View>
   )
 }
