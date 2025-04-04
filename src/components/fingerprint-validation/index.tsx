ae543db11f78d0879ee61163395005f020193d7b
Willian Alves Moreno <willian_moreno@outlook.com>
2025-04-04 12:47:27 -0300

    refactor: Ajustando layout componente fingerprint validation

--- src/components/fingerprint-validation/index.tsx
+++ src/components/fingerprint-validation/index.tsx
@@ -1,5 +1,8 @@
+import LogoSvg from '@assets/logo.svg'
 import { AnchorButton } from '@components/anchor-button'
 import { Button } from '@components/button'
+import { Footer } from '@components/footer'
+import { KeyboardAvoiding } from '@components/keyboard-avoiding'
 import { useAuth } from '@hooks/use-auth'
 import { useBiometrics } from '@hooks/use-biometrics'
 import FingerprintOutlined from '@material-symbols/svg-600/outlined/fingerprint.svg'
@@ -53,17 +56,31 @@ export function FingerprintValidation({ onSuccess }: Props) {
   }
 
   return (
-    <View className="flex-1 gap-y-6">
-      <View className="flex-1 items-center justify-center gap-y-6">
-        <FingerprintOutlined className="pointer-events-none h-28 w-28 fill-sky-400 leading-none" />
-        <Text className="text-center font-sans-bold text-3xl text-sky-900">
-          Use a biometria para desbloquear o app
-        </Text>
-      </View>
-      <View className="gap-y-6">
-        <Button value="Usar biometria" disabled={!isFingerprintAvailable} onPress={handleSignIn} />
-        <AnchorButton value="Sair da conta" className="mx-auto" onPress={handleSignOut} />
-      </View>
-    </View>
+    <KeyboardAvoiding.Root className="absolute inset-0 z-10 h-svh w-svw flex-1 bg-sky-50">
+      <KeyboardAvoiding.View>
+        <KeyboardAvoiding.Scroll>
+          <View className="mx-auto flex w-52">
+            <LogoSvg height={150} style={{ marginTop: 'auto', marginBottom: 'auto' }} />
+          </View>
+          <View className="flex-1 gap-y-6">
+            <View className="flex-1 items-center justify-center gap-y-6">
+              <FingerprintOutlined className="pointer-events-none h-28 w-28 fill-sky-400 leading-none" />
+              <Text className="text-center font-sans-bold text-3xl text-sky-900">
+                Use a biometria para desbloquear o app
+              </Text>
+            </View>
+            <View className="gap-y-6">
+              <Button
+                value="Usar biometria"
+                disabled={!isFingerprintAvailable}
+                onPress={handleSignIn}
+              />
+              <AnchorButton value="Sair da conta" className="mx-auto" onPress={handleSignOut} />
+            </View>
+          </View>
+          <Footer variant="diluted" />
+        </KeyboardAvoiding.Scroll>
+      </KeyboardAvoiding.View>
+    </KeyboardAvoiding.Root>
   )
 }
